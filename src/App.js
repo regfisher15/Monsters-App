28044b3e29864e9d22ece30b0d7379d716958250
rbfisher15 <rbfisher@crimson.ua.edu>
2024-08-08 09:23:54 -0500

    Replace hard coded values and fetch new values from external api

--- src/App.js
+++ src/App.js
@@ -8,25 +8,19 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Linda',
-          id: 1
-        },
-        {
-          name: 'Frank',
-          id: 2
-        },
-        {
-          name: 'Jacky',
-          id: 3
-        },
-        {
-          name: 'James', 
-          id: 4
-        },
-      ]
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return {monsters: users}
+      },
+      () => {
+        console.log(this.state);
+      }))
   }
 
   render() {
