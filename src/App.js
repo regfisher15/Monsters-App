1844fdbc5952f6d893ddd1cf985ee1f50564232b
rbfisher15 <rbfisher@crimson.ua.edu>
2024-08-12 14:00:02 -0500

    Convert class components to functional components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
+const App = () => {
+
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  //call another useeffect to make filteredMonsters more efficient
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event) => {
+    //convert the typed string to lowercase
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  }
+
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters App</h1>
+
+      <SearchBox className='monsters-search-box' onChangeHandler={onSearchChange} placeholder='search monsters' />
+      <CardList monsters={filteredMonsters} /> 
+    </div>
+  );
+}
+
+/*
 class App extends Component {
   constructor() {
     super();
@@ -49,5 +87,6 @@ class App extends Component {
     );
   }
 }
+  */
 
 export default App;
